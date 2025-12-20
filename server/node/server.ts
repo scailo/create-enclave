d987b7e84fbbc0d2e4d1cb938377445572299c54
Pratheek Adidela <apratheek@unaxiom.com>
2025-12-20 14:28:50 +0530

    Updated server.ts to setup redis in order to handle workflow events. Also setup encoding/decoding signed cookie handling process, including provision to add protected routes.

--- server/node/server.ts
+++ server/node/server.ts
@@ -1,15 +1,24 @@
-import Fastify from "fastify";
+import Fastify, { FastifyReply, FastifyRequest } from "fastify";
 import fastifyStatic from "@fastify/static";
 import * as path from "path";
 import * as fs from "fs";
 import { createConnectTransport } from "@connectrpc/connect-node";
-import { getScailoClientForLoginService } from "@kernelminds/scailo-sdk";
+import { BOOL_FILTER, getScailoClientForLoginService, getScailoClientForPurchasesOrdersService, getScailoClientForVaultService, getScailoClientForVendorsService, WorkflowEvent } from "@kernelminds/scailo-sdk";
+import { createClient } from "redis";
+import type { FastifyCookieOptions } from "@fastify/cookie";
+import cookie from "@fastify/cookie";
 
 const ENCLAVE_NAME = (process.env.ENCLAVE_NAME || "").trim();
 const SCAILO_API = (process.env.SCAILO_API || "").trim();
 const PORT = parseInt(process.env.PORT || "0");
 const USERNAME = (process.env.USERNAME || "").trim();
 const PASSWORD = (process.env.PASSWORD || "").trim();
+const REDIS_USERNAME = (process.env.REDIS_USERNAME || "").trim();
+const REDIS_PASSWORD = (process.env.REDIS_PASSWORD || "").trim();
+const REDIS_URL = (process.env.REDIS_URL || "").trim();
+const WORKFLOW_EVENTS_CHANNEL = (process.env.WORKFLOW_EVENTS_CHANNEL || "").trim();
+/**If this has to be a constant, this can be overridden by creating an environment variable */
+const COOKIE_SIGNATURE_SECRET = (process.env.COOKIE_SIGNATURE_SECRET || "").trim();
 
 if (ENCLAVE_NAME == undefined || ENCLAVE_NAME == null || ENCLAVE_NAME == "") {
     console.log("ENCLAVE_NAME not set");
@@ -36,20 +45,59 @@ if (PASSWORD == undefined || PASSWORD == null || PASSWORD == "") {
     process.exit(1);
 }
 
+if (REDIS_URL == undefined || REDIS_URL == null || REDIS_URL == "") {
+    console.log("REDIS_URL not set");
+    process.exit(1);
+}
+
+if (WORKFLOW_EVENTS_CHANNEL == undefined || WORKFLOW_EVENTS_CHANNEL == null || WORKFLOW_EVENTS_CHANNEL == "") {
+    console.log("WORKFLOW_EVENTS_CHANNEL not set");
+    process.exit(1);
+}
+
+if (COOKIE_SIGNATURE_SECRET == undefined || COOKIE_SIGNATURE_SECRET == null || COOKIE_SIGNATURE_SECRET == "") {
+    console.log("COOKIE_SIGNATURE_SECRET not set");
+    process.exit(1);
+}
+
+const redisConnectionString = `redis://${REDIS_USERNAME}:${REDIS_PASSWORD}@${REDIS_URL}`;
+
+async function setupWorkflowEventsCapture() {
+    const client = await createClient({ url: redisConnectionString })
+        .on('error', err => server.log.info('Redis Client Error', err))
+        .connect();
+    client.subscribe(WORKFLOW_EVENTS_CHANNEL, (incomingMessage) => {
+        const message = JSON.parse(incomingMessage) as WorkflowEvent;
+        console.log("Received Workflow Event: " + JSON.stringify(message));
+    });
+}
+
 function getTransport(apiEndPoint: string) {
     return createConnectTransport({
-        baseUrl: apiEndPoint, httpVersion: "1.1", useBinaryFormat: false, interceptors: [
-            // appendAuthToken
-        ]
+        baseUrl: apiEndPoint, httpVersion: "1.1", useBinaryFormat: false, interceptors: []
     });
 }
 
 const transport = getTransport(SCAILO_API);
-const server = Fastify({ logger: true, trustProxy: true });
+const server = Fastify({
+    trustProxy: true,
+    logger: {
+        level: 'info',
+        transport: {
+            target: 'pino-pretty'
+        }
+    }
+});
 const loginClient = getScailoClientForLoginService(transport);
+const vaultClient = getScailoClientForVaultService(transport);
+const purchaseOrdersClient = getScailoClientForPurchasesOrdersService(transport);
+const vendorsClient = getScailoClientForVendorsService(transport);
 
 let authToken = "";
 let production = false;
+if (process.env.PRODUCTION && process.env.PRODUCTION == "true") {
+    production = true;
+}
 
 const enclavePrefix = `/enclave/${ENCLAVE_NAME}`;
 
@@ -68,6 +116,12 @@ async function loginToAPI() {
         }, 3600 * 1000);
     }
 }
+
+/**Register the cookie plugin */
+server.register(cookie, {
+    secret: COOKIE_SIGNATURE_SECRET, // for cookies signature
+    parseOptions: {}     // options for parsing cookies
+} as FastifyCookieOptions)
 
 // ------------------------------------------------------------------------------------------
 // Register static routes here (this will serve the correct favicon from any route)
@@ -105,6 +159,84 @@ server.get(`${enclavePrefix}/ui/*`, async (request, reply) => {
     reply.send(replaceBundleCaches(indexPage));
 });
 
+function appendDefaultHeader({ authTokenToAdd }: { authTokenToAdd?: string }) {
+    if (!authTokenToAdd) {
+        authTokenToAdd = authToken;
+    }
+    return {
+        "auth_token": authTokenToAdd
+    }
+}
+
+/**Handle the ingress -> sets the auth token and redirects to the entry point */
+server.get(`${enclavePrefix}/ingress/:token`, async (request, reply) => {
+    try {
+        if (!production) {
+            // In dev, use the default auth token
+            reply.setCookie(`${ENCLAVE_NAME}_auth_token`, authToken, {
+                path: "/",
+                signed: true,
+                expires: new Date(Date.now() + 3600 * 1000)
+            });
+        } else {
+            const token = (<any>request.params).token;
+            if (!token) {
+                reply.status(400).send("Missing token");
+                return;
+            }
+            // Correctly verify the ingress token
+            const ingress = await vaultClient.verifyEnclaveIngress({ token }, { headers: appendDefaultHeader({ authTokenToAdd: authToken }) });
+            reply.setCookie(`${ENCLAVE_NAME}_auth_token`, ingress.authToken, {
+                path: "/",
+                signed: true,
+                expires: new Date(parseInt(ingress.expiresAt.toString()) * 1000)
+            });
+        }
+
+        reply.redirect(`${enclavePrefix}/ui`);
+    } catch (e) {
+        reply.code(500).send(e);
+    }
+});
+
+/**Handles the protected routes -> verifies the auth token and calls the handler */
+async function handleProtectedRoute(request: FastifyRequest, reply: FastifyReply, handler: (userAuthToken: string) => void) {
+    try {
+        let cookieValue = request.cookies[`${ENCLAVE_NAME}_auth_token`];
+        if (!cookieValue || cookieValue == "") {
+            server.log.error("No auth token found");
+            reply.redirect(`${enclavePrefix}/ui`);
+        }
+        const userAuthToken = request.unsignCookie(cookieValue!).value;
+        if (!userAuthToken || userAuthToken == "") {
+            server.log.error("No auth token found");
+            reply.redirect(`${enclavePrefix}/ui`);
+        }
+
+        handler(userAuthToken!);
+    } catch (e) {
+        reply.code(500).send(e);
+    }
+}
+
+server.get(`${enclavePrefix}/protected/api/random`, async (request, reply) => {
+    handleProtectedRoute(request, reply, async (userAuthToken) => {
+        const [purchaseOrdersList, vendorsList] = await Promise.all([
+            purchaseOrdersClient.filter({
+                isActive: BOOL_FILTER.BOOL_FILTER_TRUE,
+                count: BigInt(5),
+            }, { headers: appendDefaultHeader({ authTokenToAdd: userAuthToken! }) }),
+
+            vendorsClient.filter({
+                isActive: BOOL_FILTER.BOOL_FILTER_TRUE,
+                count: BigInt(5),
+            }, { headers: appendDefaultHeader({ authTokenToAdd: userAuthToken! }) })
+        ]);
+
+        reply.send({ random: Math.random(), purchaseOrders: purchaseOrdersList.list, vendors: vendorsList.list });
+    });
+})
+
 server.get(`${enclavePrefix}/api/random`, async (request, reply) => {
     reply.header("Content-Type", "application/json");
     reply.send({ random: Math.random() });
@@ -137,6 +269,7 @@ server.setNotFoundHandler((request, reply) => {
 // ------------------------------------------------------------------------------------------
 
 // ------------------------------------------------------------------------------------------
+setupWorkflowEventsCapture();
 console.log(`Listening on port ${PORT} with Production: ${production}`);
 loginToAPI();
 server.listen({ port: PORT, host: '0.0.0.0' });
